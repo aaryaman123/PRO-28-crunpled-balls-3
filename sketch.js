37745c0dcd36a45c109b403ab89d90aeadcf3b6b
aaryaman123 <66904628+aaryaman123@users.noreply.github.com>
2020-09-01 12:59:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var ldust, rdust, bdust;
+var ground;
+var paper;
+var img,img1;
+var launcher;
+
+function preload()
+{
+	img1 = loadImage("dustbin.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	
+	engine = Engine.create();
+	world = engine.world;
+
+	var options = {
+		isStatic : true
+	}
+
+	//Create the Bodies Here.
+	ldust = new Trash(500,630,10,100,options);
+	rdust = new Trash(700,630,10,100,options);
+	World.add(world, rdust);
+
+	ground = Bodies.rectangle( 400,690,800,20,options);
+	World.add(world, ground);
+
+	paper = new Paper(20,400,20);
+
+	launcher = new Launcher(paper.body,{x:100,y:250});
+
+	img = createSprite(600,600,10,10);
+	img.addImage(img1);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background(225);
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y,800,20);
+
+  ldust.visible = false;
+  ldust.visible = false;
+  img.scale = 0.5;
+  paper.scale = 1;
+  
+ paper.display();
+ launcher.display();
+ drawSprites();
+ 
+}
+
+function mouseDragged(){
+
+    Matter.Body.setPosition(paper.body,{x:mouseX,y:mouseY});
+  
+  }
+  
+  function mouseReleased(){
+    launcher.fly();
+  }
